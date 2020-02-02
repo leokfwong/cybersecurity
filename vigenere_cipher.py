06526b2e69bda596269344226147b1b395c75f2f
leokfwong <leo.wong2@mail.mcgill.ca>
2020-02-01 23:03:13 -0500

    Implement code to break Vigenere cipher

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -14,6 +14,7 @@ class vigenereCipher:
 	"""
 	Object containing methods to encrypt and decrypt a Vigenere cipher given a message and a key.
 	"""
+
 	# Encrypt message given a key
 	def encrypt(self, plaintext, key):
 		plaintext = plaintext.upper()
@@ -26,6 +27,7 @@ class vigenereCipher:
 				text += plaintext[i]
 				offset += 1
 		return text
+
 	# Decrypt cipher given a key
 	def decrypt(self, cipher, key):
 		text = ""
@@ -37,25 +39,46 @@ class vigenereCipher:
 				text += cipher[i]
 				offset += 1
 		return text
+
 	# Find the length of the unknown key
-	def findKeyLength(self, cipher):
+	def findKeyLength(self, cipher, verbose=False):
 		cipher = re.sub(r"[^A-Z]+", "", cipher.upper())
 		sum_array = []
+		# Iterate through potential key lengths
 		for i in range(1, int(len(cipher) / 50)):
 			current = [cipher[index] for index, value in enumerate(cipher) if index % i == 0]
 			current_text = "".join(current)
 			sc = shift_cipher.shiftCipher()
 			broken = sc.break_shift(current_text)
-			print(f"Iteration {i}, sum={broken['sum']}")
 			if i <= len(cipher) / 2:
 				sum_array.append(broken["sum"])
-		
+			if verbose:
+				print(f"Key of length {i}, sum={broken['sum']}")
+		# Fetch top closest potential lengths
 		diff_array = [abs(x - ENG_PROBABILITY) for x in sum_array]
 		top = sorted(range(len(diff_array)), key=lambda k: diff_array[k])[:5]
 		top = [x + 1 for x in top]
-		print(top)
 		min_length = min(top)
-		print(f"Potential key length is {min_length}")	
+		if verbose:
+			print(f"Potential key length is {min_length}")
+		return min_length
+
+	# Break Vigenere cipher
+	def breakVigenere(self, cipher, key_length, verbose=False):
+		cipher = re.sub(r"[^A-Z]+", "", cipher.upper())
+		broken_key = ""
+		for i in range(key_length):
+			current = [value for index, value in enumerate(cipher) if index % (key_length) == i]
+			current_text = "".join(current)
+			sc = shift_cipher.shiftCipher()
+			broken = sc.break_shift(current_text, verbose=False)
+			broken_key += dict_index[broken["k"]]
+			if verbose:
+				print(f"Iteration {i}, k={broken['k']}, ({dict_index[broken['k']]})")
+		if verbose:
+			print(f"The key is {broken_key}")
+		return broken_key
+
 
 def readTxtString(path):
 	with open(path, "r") as f:
@@ -63,27 +86,24 @@ def readTxtString(path):
 		plaintext = re.sub(r'\s+', ' ', plaintext)
 		return plaintext
 
-
-# Plaintext and key
-plaintext = "WE, THEREFORE, THE REPRESENTATIVES OF THE UNITED STATES OF AMERICA, IN GENERAL CONGRESS, ASSEMBLED, APPEALING TO THE SUPREME JUDGE OF THE WORLD FOR THE RECTITUDE OF OUR INTENTIONS, DO, IN THE NAME, AND BY AUTHORITY OF THE GOOD PEOPLE OF THESE COLONIES, SOLEMNLY PUBLISH AND DECLARE, THAT THESE UNITED COLONIES ARE, AND OF RIGHT OUGHT TO BE FREE AND INDEPENDENT STATES, THAT THEY ARE ABSOLVED FROM ALL ALLEGIANCE TO THE BRITISH CROWN, AND THAT ALL POLITICAL CONNECTION BETWEEN THEM AND THE STATE OF GREAT BRITAIN, IS AND OUGHT TO BE TOTALLY DISSOLVED, AND THAT AS FREE AND INDEPENDENT STATES, THEY HAVE FULL POWER TO LEVY WAR, CONCLUDE PEACE, CONTRACT ALLIANCES, ESTABLISH COMMERCE, AND TO DO ALL OTHER ACTS AND THINGS WHICH INDEPENDENT STATES MAY OF RIGHT DO. AND FOR THE SUPPORT OF THIS DECLARATION, WITH A FIRM RELIANCE ON THE PROTECTION OF DIVINE PROVIDENCE, WE MUTUALLY PLEDGE TO EACH OTHER OUR LIVES, OUR FORTUNES AND OUR SACRED HONOR. Mr. Bennet was among the earliest of those who waited on Mr. Bingley. He had always intended to visit him, though to the last always assuring his wife that he should not go; and till the evening after the visit was paid she had no knowledge of it. It was then disclosed in the following manner. Observing his second daughter employed in trimming a hat, he suddenly addressed her with: I am sorry to hear that; but why did not you tell me that before? If I had known as much this morning I certainly would not have called on him. It is very unlucky; but as I have actually paid the visit, we cannot escape the acquaintance now.” The astonishment of the ladies was just what he wished; that of Mrs. Bennet perhaps surpassing the rest; though, when the first tumult of joy was over, she began to declare that it was what she had expected all the while. “How good it was in you, my dear Mr. Bennet! But I knew I should persuade you at last. I was sure you loved your girls too well to neglect such an acquaintance. Well, how pleased I am! and it is such a good joke, too, that you should have gone this morning and never said a word about it till now.” “Now, Kitty, you may cough as much as you choose,” said Mr. Bennet; and, as he spoke, he left the room, fatigued with the raptures of his wife. “What an excellent father you have, girls!” said she, when the door was shut. “I do not know how you will ever make him amends for his kindness; or me, either, for that matter. At our time of life it is not so pleasant, I can tell you, to be making new acquaintances every day; but for your sakes, we would do anything. Lydia, my love, though you are the youngest, I dare say Mr. Bingley will dance with you at the next ball.”"
-key = "UNITEDSTATES"
-
-#plaintext = readTxtString("test.txt")
-#key = "MAMBA"
-print(len(plaintext))
+# Load plaintext and set key
+plaintext = readTxtString("plaintext/test.txt")
+key = "MAMBA"
 
 # Create vigenere object
-tmp = vigenereCipher()
+vig = vigenereCipher()
 
 # Encrypt plaintext
-encrypted_message = tmp.encrypt(plaintext, key)
-#print(f"The encrypted message is {encrypted_message}")
-print(len(encrypted_message))
+encrypted_message = vig.encrypt(plaintext, key)
+print(f"The encrypted message is {encrypted_message}")
 
 # Decrypt cipher
-decrypted_message = tmp.decrypt(encrypted_message, key)
-#print(f"The decrypted message is {decrypted_message}")
-print(len(decrypted_message))
+decrypted_message = vig.decrypt(encrypted_message, key)
+print(f"The decrypted message is {decrypted_message}")
 
-encrypted_message = readTxtString("cipher.txt")
-tmp.findKeyLength(encrypted_message)
+# Load cipher with unknown key
+cipher = readTxtString("cipher/cipher.txt")
+# Break Vigenere cipher
+key_length = vig.findKeyLength(cipher, verbose=True)
+broken_key = vig.breakVigenere(cipher, key_length, verbose=True)
+
